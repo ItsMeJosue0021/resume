b00ce4656c0e7b036fbae46aac9a10b034bd2b05
Joshua Salceda <100130292+ItsMeJosue0021@users.noreply.github.com>
2023-05-16 11:07:42 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+import 'boxicons';
+
+AOS.init({
+    offset: 150,
+    duration: 1400
+});
+
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'rblack': '#211F2A',
+        'lgray': '#E6E6E6',
+        'rblue': '#1576A4'
+      } 
+    },
+  },
+  plugins: [],
+}
+
